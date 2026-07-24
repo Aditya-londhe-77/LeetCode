7ccac257739a11bf76f3272b54dcfdda595a38f0
Aditya-londhe-77 <londheaditya1212@gmail.com>
2026-07-24 22:37:19 +0530

    Time: 0 ms (100%), Space: 42.2 MB (85.22%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,11 @@
-class Solution {
+public class Solution {
     public int reverseBits(int n) {
-        String str = Integer.toBinaryString(n);
-        int m = str.length();
-        for(int i=0; i<32-m;i++){
-            str='0'+str;
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>>= 1;
         }
-        String newstr = "";
-        for(int i=str.length()-1; i>=0; i--){
-            newstr+=str.charAt(i);
-        }
-        return Integer.parseInt(newstr,2);
+        return res;
     }
 }
