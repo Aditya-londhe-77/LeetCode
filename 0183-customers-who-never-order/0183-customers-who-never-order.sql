060a9fa03c1a1737d9bbb51d0276bf59da5f62fc
Aditya-londhe-77 <londheaditya1212@gmail.com>
2026-07-28 23:45:45 +0530

    Time: 1395 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,6 +1,6 @@
-SELECT c.name AS Customers
-FROM Customers c
-WHERE id NOT IN(
-    SELECT customerId
-    FROM Orders
-);
+
+SELECT name AS Customers
+FROM Customers
+LEFT JOIN Orders
+ON Customers.id = Orders.customerId
+WHERE Orders.customerId IS NULL;
