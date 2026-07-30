b324f38d9d8577cf2512b6fd266e22fdc61ef1a5
Aditya-londhe-77 <londheaditya1212@gmail.com>
2026-07-30 22:28:59 +0530

    Time: 1 ms (76.88%), Space: 42.7 MB (11.83%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,15 +1,22 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n==1 || n==7) return true;
-        else if(n<10) return false;
-        else{
-            int sum=0;
-            while(n>0){
-                int temp=n%10;
-                sum+= temp*temp;
-                n=n/10;
-            }
-            return isHappy(sum);
-        }
+        HashSet<Integer> set = new HashSet<>();
+		
+		while (!set.contains(n)) {
+			set.add(n);
+			int answerTillNow = 0;
+			int num = n; 
+			while (num > 0) {
+				int digit = num % 10;
+				answerTillNow = answerTillNow + (digit * digit);
+				num = num/10;
+			}
+			n = answerTillNow;
+		}
+		if (n == 1) {
+			return true;
+		}
+
+		return false;
     }
 }
