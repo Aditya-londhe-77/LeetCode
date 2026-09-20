7700ad13362a8a2286261537171458e6a2cd9b7d
Aditya-londhe-77 <londheaditya1212@gmail.com>
2026-09-20 22:31:53 +0530

    Time: 0 ms (100%), Space: 43.5 MB (58.23%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,27 @@
+//Optimal solution with dutch flag problem and three pointers 
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0;
+            int mid=0;
+            int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp;
+                temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;                
+            }
+            else if(nums[mid]==2){
+                int temp1;
+                temp1=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp1;
+                high--;
+            }
+        }        
+    }
+}
